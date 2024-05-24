97dde027dff84a8e48733fbd629900b3f9917f61
julio.cruz <julio.cruz@g.globo>
2024-05-24 10:41:11 -0300

    Limpeza de código e itens de projeto

--- trato-tech/src/index.js
+++ trato-tech/src/index.js
@@ -6,7 +6,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      Hello world
+      Hellow world
     </div>
   </React.StrictMode>
 );
+
