5b13b3e91a4eeb5b1d61c0f7a185675a20d3ee1c
julio.cruz <julio.cruz@g.globo>
2024-05-24 11:31:15 -0300

    Criando do app

---
+++ trato-tech/src/components/Navbar/index.js
@@ -0,0 +1,36 @@
+import styles from './Navbar.module.scss'
+import { ReactComponent as Logo} from '../../assets/logo.svg'
+import classNames from 'classnames'
+import { RiShoppingCart2Line, RiShoppingCartFill } from 'react-icons/ri'
+
+const iconeProps = {
+    color: 'white',
+    size: 24
+}
+
+export default function Navbar(){
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.logo}>
+                <div>
+                    <a href='/' className={classNames(styles.links, {
+                        [styles.selected]: window.location.pathname === '/'
+                    })}>
+                        Página inicial
+                    </a>
+                </div>
+            </div>
+            <div className={styles.busca}>
+
+            </div>
+            <div className={styles.icontes}>
+                <a href="/carrinho">
+                    {window.location.pathname === '/carrinho'
+                        ? <RiShoppingCartFill {...iconeProps} />
+                        : <RiShoppingCart2Line {...iconeProps} />
+                    }
+                </a>
+            </div>
+        </nav>
+    )
+}
--- trato-tech/src/index.js
+++ trato-tech/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      Hellow world
+      <Navbar/>
     </div>
   </React.StrictMode>
 );
